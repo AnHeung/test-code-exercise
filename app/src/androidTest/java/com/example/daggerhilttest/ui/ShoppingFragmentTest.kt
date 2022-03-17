93471f9b05408f44f4998d4996b8abf22eef4212
gskebin <gskebin4@gmail.com>
2022-03-17 17:40:01 +0900

    chapter10 - Navigation 테스트를 위한 nav_graph 추가 - AddShoppingItemFragmentTest, ShoppingFragmentTest 클래스 추가 및 ui 테스트 코드 작성

---
+++ app/src/androidTest/java/com/example/daggerhilttest/ui/ShoppingFragmentTest.kt
@@ -0,0 +1,47 @@
+package com.example.daggerhilttest.ui
+
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import com.example.daggerhilttest.R
+import com.example.daggerhilttest.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.*
+
+@MediumTest
+@HiltAndroidTest
+@ExperimentalCoroutinesApi
+class ShoppingFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun clickAddShoppingItemButton_navigateToAddShoppingItemFragment() {
+        val navController = mock(NavController::class.java)
+//        `when`(navController.popBackStack()).thenReturn()
+        launchFragmentInHiltContainer<ShoppingFragment> {
+            Navigation.setViewNavController(requireView(), navController)
+        }
+
+        onView(withId(R.id.fabAddShoppingItem)).perform(click())
+
+        verify(navController).navigate(
+            ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
+        )
+    }
+
+}
